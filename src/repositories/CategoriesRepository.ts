f26b30e9153ce156b7aba7d12e9970b32195c30b
Renato Marinho <renato.marinhofr@gmail.com>
2021-07-15 17:23:50 -0300

    Validando cadastro de categoria

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -34,5 +34,10 @@ class CategoriesRepository {
     return this.categories;
   }
 
+  findByName(name: string): Category{
+    const category = this.categories.find(category => category.name === name);
+    return category;
+  }
+
 }
 export { CategoriesRepository }
