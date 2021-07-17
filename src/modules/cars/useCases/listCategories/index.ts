312ccea3b84ba1661aa3f2c56a2972f520ba4ae7
Renato Marinho <renato.marinhofr@gmail.com>
2021-07-17 10:46:05 -0300

    Separando os repositórios

--- src/modules/cars/useCases/listCategories/index.ts
+++ src/modules/cars/useCases/listCategories/index.ts
@@ -1,4 +1,4 @@
-import { CategoriesRepository } from "../../repositories/CategoriesRepository";
+import { CategoriesRepository } from "../../repositories/implemetations/CategoriesRepository";
 import { ListCategoriesController } from "./ListCategoriesController";
 import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 
